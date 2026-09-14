434d42d1ed1e7dfdac46bb8ac648ff16d13056e3
ShivaCoreDev <michael.worob@gmail.com>
2026-09-15 00:54:14 +0200

    feat(memory): export kernel stack lifecycle

--- modules/atc-shivacore/kernel/src/lib.rs
+++ modules/atc-shivacore/kernel/src/lib.rs
@@ -36,6 +36,8 @@ pub mod x86_64_user_mapping;
 pub mod x86_64_context_switch;
 #[cfg(feature = "x86-boot")]
 pub mod process_context;
+#[cfg(feature = "x86-boot")]
+pub mod kernel_stack;
 pub mod atcfs;
 pub mod vfs;
 pub mod syscall;
